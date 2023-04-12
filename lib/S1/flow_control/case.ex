24dbd0a54c10de7bb9133711caaa9b6dd729b16e
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-04-12 16:08:37 -0500

    Getting started with Elixir - MC #2

---
+++ lib/S1/flow_control/case.ex
@@ -0,0 +1,14 @@
+x = IO.gets("Insert a number: ") #"5\n"
+    |> String.trim() #"5"
+    |> String.to_integer() #5
+
+#x = String.to_integer(String.trim(IO.gets("Insert a number: ")))
+
+case {1, 2, 3} do
+  {4, 5, 6} ->
+    IO.puts("It will not match")
+  {1, ^x, 3 } ->
+    IO.puts("It matches if x == 2")
+  #Default case if none will match
+  _ -> IO.puts("None matching")
+end
