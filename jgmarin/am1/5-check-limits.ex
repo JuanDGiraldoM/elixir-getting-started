d0805394cfa6ecd65f94ea023f4cdc4fa763d5a5
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2023-04-17 08:52:24 -0500

    Unify code

--- jgmarin/am1/5-check-limits.ex
+++ jgmarin/am1/5-check-limits.ex
@@ -1,10 +1,22 @@
 defmodule CheckNumber do
   @moduledoc """
-  Utilice el flujo de control cond para comprobar si una variable x es mayor que 10, menor que 5, o entre 5 y 10.
+  5. Utiliza el flujo de control if/else para comprobar si una variable x es mayor que 10.
+   Si lo es, imprime "x es mayor que 10",
+   de lo contrario imprime "x es menor o igual que 10".
+
+  6. Utilice el flujo de control cond para comprobar si una variable x es mayor que 10, menor que 5, o entre 5 y 10.
   Si lo es, imprima "x es mayor que 10",
   en caso contrario imprima "x es menor o igual que 10".
   Imprima "x es mayor que 10", "x es menor que 5", o "x está entre 5 y 10" dependiendo del valor de x.
   """
+
+  def checkNumberLimit(number, limit) do
+    if number > limit do
+      IO.puts("#{number} es mayor que #{limit}")
+    else
+      IO.puts("#{number} es menor o igual que #{limit}")
+    end
+  end
 
   def checkNumberLimit(number, upper_limit, lower_limit) do
     cond do
@@ -17,4 +29,5 @@ defmodule CheckNumber do
 end
 
 number = IO.gets("Enter a number: ") |> String.trim() |> String.to_integer()
+CheckNumber.checkNumberLimit(number, 10)
 CheckNumber.checkNumberLimit(number, 10, 5)
